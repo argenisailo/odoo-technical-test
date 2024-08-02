422dec77b477cdd47ab12e9e55e012d08d3fdfd6
argenisailo <lopezargenisai@gmail.com>
2024-08-02 12:43:31 -0600

    Cambios menores

--- models/to_do_list.py
+++ models/to_do_list.py
@@ -2,6 +2,7 @@ from odoo import models, fields, api
 
 class ToDoList(models.Model):
     _name = 'to.do.list'
+    _description = 'To-do List'
 
     title = fields.Char(string='Title')
     description = fields.Text(string='Description')
